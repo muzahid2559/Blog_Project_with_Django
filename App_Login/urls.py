ec69ccbfe0aa2105410a7a8c324e95f057781ac0
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-11-05 17:22:40 +0600

    User Profile Part-1

--- App_Login/urls.py
+++ App_Login/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('signup/', views.sign_up, name='signup'),
     path('login/', views.login_page, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('change-profile/', views.user_change, name='user_change'),
+    path('password/', views.pass_change, name='pass_change'),
 
 ]
