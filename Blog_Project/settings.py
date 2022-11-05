3cc351eef2b917754bdeeb8eb903f1313c9bcc73
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-11-05 08:47:22 +0600

    User Sign Up

--- Blog_Project/settings.py
+++ Blog_Project/settings.py
@@ -31,7 +31,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'App_Blog',
     'App_Login',
+    'crispy_forms',
 ]
+
+
+# bootstrap, bootstrap3, bootstrap4, uni-form
+CRISPY_TEMPLATE_PACK= 'bootstrap4'
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
