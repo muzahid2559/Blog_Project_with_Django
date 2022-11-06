86082cd89f186d9663650782f98a80036c7c3142
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-11-06 12:39:51 +0600

    Blog Homepage

--- App_Blog/views.py
+++ App_Blog/views.py
@@ -8,9 +8,6 @@ import uuid
 
 
 # Create your views here.
-def blog_list(request):
-    return render(request,'App_Blog/blog_list.html', context={})
-
 
 class CreateBlog(LoginRequiredMixin, CreateView):
     model = Blog
@@ -24,3 +21,9 @@ class CreateBlog(LoginRequiredMixin, CreateView):
         blog_obj.slug = title.replace(" ", "-") + "-" + str(uuid.uuid4())
         blog_obj.save()
         return HttpResponseRedirect(reverse('index'))
+
+
+class BlogList(ListView):
+    context_object_name = 'blogs'
+    model = Blog
+    template_name = 'App_Blog/blog_list.html'
